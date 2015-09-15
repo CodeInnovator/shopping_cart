7fdab386265d0baeb2ee0cce759ecf9c9abc8538
thomas <mail2thomaskim@gmail.com>
2015-09-15 01:18:13 -0700

    add empty function to the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
   def index
@@ -28,7 +28,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
+        format.html { redirect_to @cart}
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new }
@@ -54,13 +54,14 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to store_url, notice: 'Your cart is currently empty'}
+      format.json {head :no_conent}
     end
-  end
-
+    end
+    
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
@@ -71,4 +72,9 @@ class CartsController < ApplicationController
     def cart_params
       params[:cart]
     end
+
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+      end
 end
